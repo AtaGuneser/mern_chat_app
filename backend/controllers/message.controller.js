0964a7c470d16a7c0cd832bebea86406f58323d5
Ata GÜNEŞER <ataguneser@hotmail.com>
2025-01-13 12:49:16 +0300

    update

---
+++ backend/controllers/message.controller.js
@@ -0,0 +1,9 @@
+export const sendMessage = async (req, res) => {
+  try {
+    const { message } = req.body
+    const { id } = req.params
+    const senderId = req.user._id
+  } catch (error) {
+    res.status(500).json({ message: 'Internal Server Error' })
+  }
+}
