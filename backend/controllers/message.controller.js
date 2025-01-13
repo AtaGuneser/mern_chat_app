c5e7db7ed50439b8056cf17e6a0c9c02f64fc533
Ata GÜNEŞER <ataguneser@hotmail.com>
2025-01-13 16:50:25 +0300

    update

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,8 +1,14 @@
+import Conversation from '../models/conversation.model.js'
+
 export const sendMessage = async (req, res) => {
   try {
     const { message } = req.body
-    const { id } = req.params
+    const { id: receiverId } = req.params
     const senderId = req.user._id
+
+    await Conversation.findOne({
+      participants: { $all: [senderId, receiverId] }
+    })
   } catch (error) {
     res.status(500).json({ message: 'Internal Server Error' })
   }
