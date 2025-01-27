769157c832c886bdeb4a476378f04668172abfdf
Ata GÜNEŞER <ataguneser@hotmail.com>
2025-01-27 12:59:28 +0300

    Add React Router and implement basic routing

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,7 +1,9 @@
+import { Link } from 'react-router-dom'
+
 const Login = () => {
   return (
     <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
-      <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-0 '>
+      <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-0 '>
         <h1 className='text-3xl font-semibold text-center text-gray-300'>
           Login
           <span className='text-blue-500'> ChatApp</span>
@@ -32,12 +34,12 @@ const Login = () => {
             />
           </div>
 
-          <a
-            href='#'
+          <Link
+            to='/signup'
             className='text-sm hover:underline hover:text-blue-600 mt-2 inline-block'
           >
             Don&apos;t have an account? Register
-          </a>
+          </Link>
 
           <div>
             <button className='btn btn-primary btn-sm mt-2'>Login</button>
--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -1,15 +1,32 @@
+import { Link } from 'react-router-dom'
 import GenderCheckbox from './GenderCheckbox'
-
+import { useState } from 'react'
 const SignUp = () => {
+  const [input, setInput] = useState({
+    fullName: '',
+    username: '',
+    password: '',
+    confirmPassword: '',
+    gender: ''
+  })
+
+  const handleCheckboxChange = gender => {
+    setInput({ ...input, gender })
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    console.log(input)
+  }
   return (
     <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
-      <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-0 '>
+      <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-0 '>
         <h1 className='text-3xl font-semibold text-center text-gray-300'>
           Sign Up
           <span className='text-blue-500'> ChatApp</span>
         </h1>
 
-        <form action=''>
+        <form action='' onSubmit={handleSubmit}>
           <div>
             <label className='label p-2'>
               <span className='text-base label-text'>Full Name</span>
@@ -19,6 +36,8 @@ const SignUp = () => {
               name='fullName'
               placeholder='Enter your full name'
               className='input input-bordered w-full h-10'
+              value={input.fullName}
+              onChange={e => setInput({ ...input, fullName: e.target.value })}
             />
           </div>
 
@@ -31,6 +50,8 @@ const SignUp = () => {
               name='username'
               placeholder='Enter your username'
               className='input input-bordered w-full h-10'
+              value={input.username}
+              onChange={e => setInput({ ...input, username: e.target.value })}
             />
           </div>
 
@@ -43,6 +64,8 @@ const SignUp = () => {
               name='password'
               placeholder='Enter your password'
               className='input input-bordered w-full h-10'
+              value={input.password}
+              onChange={e => setInput({ ...input, password: e.target.value })}
             />
           </div>
 
@@ -55,17 +78,24 @@ const SignUp = () => {
               name='confirmPassword'
               placeholder='Confirm your password'
               className='input input-bordered w-full h-10'
+              value={input.confirmPassword}
+              onChange={e =>
+                setInput({ ...input, confirmPassword: e.target.value })
+              }
             />
           </div>
 
-          <GenderCheckbox />
+          <GenderCheckbox
+            handleCheckboxChange={handleCheckboxChange}
+            selectedGender={input.gender}
+          />
 
-          <a
-            href='#'
+          <Link
+            to='/login'
             className='text-sm hover:underline hover:text-blue-600 mt-2 inline-block'
           >
             Already have an account? Login
-          </a>
+          </Link>
 
           <div>
             <button className='btn btn-block border-slate-700 btn-sm mt-2'>
