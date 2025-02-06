6bad3ad5902a3637f53d561259360efafccf4f52
Ata GÜNEŞER <109044557+AtaGuneser@users.noreply.github.com>
2025-02-06 17:35:45 +0300

    Implement real-time messaging with socket.io and update conversation component

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,5 +1,6 @@
 import Conversation from '../models/conversation.model.js'
-import Message from '../models/message.modal.js'
+import Message from '../models/message.model.js'
+import { getReceiverSocketId, io } from '../socket/socket.js'
 
 export const sendMessage = async (req, res) => {
   try {
@@ -12,7 +13,7 @@ export const sendMessage = async (req, res) => {
     })
 
     if (!conversation) {
-      const conversation = await Conversation.create({
+      conversation = await Conversation.create({
         participants: [senderId, receiverId]
       })
     }
@@ -27,15 +28,23 @@ export const sendMessage = async (req, res) => {
       conversation.messages.push(newMessage._id)
     }
 
-    //await conversation.save()
-    //await newMessage.save()
+    // await conversation.save();
+    // await newMessage.save();
 
+    // this will run in parallel
     await Promise.all([conversation.save(), newMessage.save()])
+
+    // SOCKET IO FUNCTIONALITY WILL GO HERE
+    const receiverSocketId = getReceiverSocketId(receiverId)
+    if (receiverSocketId) {
+      // io.to(<socket_id>).emit() used to send events to specific client
+      io.to(receiverSocketId).emit('newMessage', newMessage)
+    }
 
     res.status(201).json(newMessage)
   } catch (error) {
-    console.log(error)
-    res.status(500).json({ message: 'Internal Server Error' })
+    console.log('Error in sendMessage controller: ', error.message)
+    res.status(500).json({ error: 'Internal server error' })
   }
 }
 
@@ -46,17 +55,15 @@ export const getMessages = async (req, res) => {
 
     const conversation = await Conversation.findOne({
       participants: { $all: [senderId, userToChatId] }
-    }).populate('messages')
+    }).populate('messages') // NOT REFERENCE BUT ACTUAL MESSAGES
 
-    if (!conversation) {
-      return res.status(200).json([])
-    }
+    if (!conversation) return res.status(200).json([])
 
     const messages = conversation.messages
 
     res.status(200).json(messages)
   } catch (error) {
-    console.log(error)
-    res.status(500).json({ message: 'Internal Server Error' })
+    console.log('Error in getMessages controller: ', error.message)
+    res.status(500).json({ error: 'Internal server error' })
   }
 }
